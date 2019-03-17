13c85906a63065a116fbe4951d1826c012ef97dc
Dustin Kempton <dkempton.developer@gmail.com>
2019-03-16 23:27:42 -0700

    Scaffold

--- parser/parser.go
+++ parser/parser.go
@@ -1,4 +1,19 @@
 package parser
 
 type Parser interface {
+	parse(path string) []Model
+}
+
+type Triangle struct {
+	V0 [3]float64
+	V1 [3]float64
+	V2 [3]float64
+	N0 [3]float64
+	N1 [3]float64
+	N2 [3]float64
+}
+
+type Model struct {
+	Name      string
+	Triangles []Triangle
 }
--- parser/stl/stl_parser.go
+++ parser/stl/stl_parser.go
@@ -1,1 +1,17 @@
 package stl
+
+import (
+	"github.com/dkkempto/freed/parser"
+)
+
+type STLParser struct {
+}
+
+func (p STLParser) parse(path string) []parser.Model {
+	return make([]parser.Model, 0)
+}
+
+func main() {
+	p := STLParser{}
+	p.parse("./res/sub2pewdiepie.stl")
+}
